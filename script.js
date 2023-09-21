b1ab763fb6a8c9d0b59dec3d0683931d9658bff4
kritikamalviya01 <kritikamalviya123@gmail.com>
2023-09-21 23:16:59 +0530

    "Added smooth Scolling JS script"

--- script.js
+++ script.js
@@ -19,5 +19,20 @@ $(document).ready(function() {
         backSpeed: 40, // Backspacing speed in milliseconds
         loop: true, // Loop the animation
     });
+
+    $(document).ready(function() {
+        // Smooth scrolling for all anchor links
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function() {
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
 });
 
